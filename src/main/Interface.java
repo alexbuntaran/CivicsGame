c30ae568b9161b15bdea9e0bf053652d2efb9b65
alexbuntaran <alexbuntaran@kingsstudent.org>
2020-10-16 19:10:02 -0700

    added manifest

--- src/main/Interface.java
+++ src/main/Interface.java
@@ -1,5 +1,27 @@
 package main;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Interface {
     
+    public Interface() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        panel.setLayout(new GridLayout(0, 4));
+		frame.add(panel, BorderLayout.CENTER);
+	    frame.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("LMAO");
+		frame.setVisible(true);
+		frame.pack();
+    }
+
 }
